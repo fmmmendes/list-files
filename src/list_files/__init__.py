94d69ee43679f0bd090badad10fa4f319cb7dace
Filipe Mendes <fmppa.mendes@gmail.com>
2023-12-03 00:57:40 +0000

    lint

--- src/list_files/__init__.py
+++ src/list_files/__init__.py
@@ -1,30 +1,19 @@
-import os 
-import sys
-import csv
+import os
+
 
 def list_files(path):
     """
     Lists all files in a directory and its subdirectories.
     """
-    
     res = []
-    
     for root, dirs, files in os.walk(path):
         for file in files:
             res_path = os.path.join(root, file)
             print(res_path)
             res.append(res_path)
-    
     return res
 
 
 def save_to_txt(list_of_files):
-    
-    # with open('output.csv', 'w', newline='') as csvfile:
-    #     writer = csv.writer(csvfile)
-        
-    #     for i in list_of_files:
-    #         writer.writerow([i])
-            
     with open('output.txt', 'w') as f:
         f.write('\n'.join(list_of_files))
--- src/list_files/__main__.py
+++ src/list_files/__main__.py
@@ -1,13 +1,11 @@
 import sys
 
 from . import list_files, save_to_txt
-#from src.list_files import list_files
+
 
 if __name__ == '__main__':
-    
+
     print('run main')
     search_path = sys.argv[1] if len(sys.argv) > 1 else "."
-    
     res = list_files(path=search_path)
-    
-    save_to_txt(list_of_files = res)
+    save_to_txt(list_of_files=res)
